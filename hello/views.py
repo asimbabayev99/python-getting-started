cd729ae984a2b359890bf13a4aa37cb14030ce4f
asimbabayev99 <asim.babayev.99@bk.ru>
2021-10-11 18:18:01 +0400

    Demo

--- hello/views.py
+++ hello/views.py
@@ -42,6 +42,7 @@ class UpdateBot(APIView):
 
     def post(self, request):
         # Сюда должны получать сообщения от телеграм и далее обрабатываться ботом
+        print("I am here")
         json_str = request.body.decode('UTF-8')
         print(json_str)
         update = types.Update.de_json(json_str)
