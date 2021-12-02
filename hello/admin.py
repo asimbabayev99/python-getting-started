327b711d7ecf1f83d674cedd751eeb64618f51ab
asimbabayev99 <asim.babayev.99@bk.ru>
2021-12-02 18:48:08 +0400

    admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Action, Keyword, Chat
 # Register your models here.
+
+admin.site.register(Action)
+admin.site.register(Keyword)
+admin.site.register(Chat)
