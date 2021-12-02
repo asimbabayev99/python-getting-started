09daff33fc4fe92263343bd567e7048e0f54d0c0
asimbabayev99 <asim.babayev.99@bk.ru>
2021-12-02 19:32:57 +0400

    db

--- hello/models.py
+++ hello/models.py
@@ -7,8 +7,8 @@ class Chat(models.Model):
     first_name = models.CharField(max_length=256, null=True, blank=True)
     last_name = models.CharField(max_length=256, null=True, blank=True)
 
-    def __str__(self):
-        return str(self.id) + "-" + self.username
+    # def __str__(self):
+    #     return str(self.id) + "-" + self.username
     
 
 
@@ -38,8 +38,8 @@ class Keyword(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, null=False, blank=False)
     name = models.CharField(max_length=100, null=False, blank=False)
 
-    def __str__(self):
-        return str(self.id) + "-" + self.name
+    # def __str__(self):
+    #     return str(self.id) + "-" + self.name
 
 
 
